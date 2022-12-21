ac30d829b12fc873393a2e05d85aa77c7cd9d4e0
Sergeant101 <s.v.zagorodnev@gmail.com>
2022-12-21 10:06:17 +0500

    Start

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TestResol.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
